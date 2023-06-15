23834a18b0227d971a6e958cb926311a5fcf1f7f
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-06-15 13:34:23 -0400

    Update colorSchemeSwitcher.js

--- colorSchemeSwitcher.js
+++ colorSchemeSwitcher.js
@@ -1,7 +1,11 @@
 var sideBySide = document.querySelectorAll('.title-and-text-plus-cta');
   
+(function(){
+
 for(var i = 0; i < sideBySide.length; i++){
-    if((sideBySide[i]).hasClass('color-scheme-none')){
+    if($(sideBySide[i]).hasClass('color-scheme-none')){
         sideBySide[i].classList.replace('color-scheme-none', 'color-scheme-blue');
     }                                    
 }
+
+})($);
